2ed0dd1e7af6613ba9560319cd77c757e0aa19fc
sigitwijk16 <107915024+sigitwijk16@users.noreply.github.com>
2024-06-03 19:31:43 +0700

    enabled cors

--- backend/src/application/web.js
+++ backend/src/application/web.js
@@ -1,9 +1,11 @@
 import express from "express";
+const cors = require("cors");
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../route/api.js";
 
 export const web = express();
+web.use(cors());
 web.use(express.json());
 web.use(errorMiddleware);
 web.use(publicRouter);
